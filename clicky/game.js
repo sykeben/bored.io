185f3260621e489a80599b030b60b6ffe50ff13e
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2019-05-17 14:29:21 -0400

    Fixed storage.

--- clicky/game.js
+++ clicky/game.js
@@ -1,7 +1,7 @@
-if (typeof localStorage.getItem('clicky.count') === undefined) localStorage.setItem('clicky.count', 0)
+if (typeof localStorage.getItem('clicky.count') === undefined) localStorage.setItem('clicky.count', '0')
 
 function countUp() {
-    localStorage.setItem('clicky.count', parseInt(localStorage.getItem('clicky.count'))+1)
+    localStorage.setItem('clicky.count', (parseInt(localStorage.getItem('clicky.count'))+1).toString())
     update()
 }
 
